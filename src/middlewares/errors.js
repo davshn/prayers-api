b43310059dd4d0efe2a03fca3b1fcec62217fc72
Hernán David Figueroa <davshn@gmail.com>
2022-03-18 12:50:00 -0500

    Control middlewares

---
+++ src/middlewares/errors.js
@@ -0,0 +1,10 @@
+module.exports.generalErrorHandler = function (err, req, res, next) {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+};
+
+module.exports.error404=function (req, res, next) {
+    res.status(404).send("Endpoint does not exist");
+};
--- src/server.js
+++ src/server.js
@@ -1,32 +1,20 @@
 const express = require('express');
 
 const routes = require('./routes/index.js');
-require("./models/index");
-
-
+const { generalErrorHandler, error404 } = require('./middlewares/errors');
+const { cors } = require('./middlewares/cors');
 
 const server = express();
 
 server.name = 'API';
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
-
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+server.use(cors);
 
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { 
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
+// Error handlers.
+server.use(error404);
+server.use(generalErrorHandler);
 
 module.exports = server;
