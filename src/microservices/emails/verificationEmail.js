cb6c846b91f086243402b8cb6fa7490070be19dc
Hernán David Figueroa <davshn@gmail.com>
2022-03-19 00:46:33 -0500

    Errores especificados

--- src/microservices/emails/verificationEmail.js
+++ src/microservices/emails/verificationEmail.js
@@ -2,8 +2,8 @@ const transporter = require("../../services/nodemailer");
 
 module.exports = function verification(mail) {
   const mailData = {
-    from: "find.spot.ar.co@gmail.com", // sender address
-    to: mail, // list of receivers
+    from: "find.spot.ar.co@gmail.com", 
+    to: mail, 
     subject: "Verifica tu cuenta",
     text: "Verifica tu cuenta",
     html: `<br> Haz click aqui para verificar tu cuenta<br/> <a href="https://find-spot.herokuapp.com/verify?id=${mail}&mail=${mail}" >Verificar</a>`,
