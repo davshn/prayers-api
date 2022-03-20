c4407293999b2d75894bac3c63cfbe3768b8f827
Hernán David Figueroa <davshn@gmail.com>
2022-03-20 13:01:49 -0500

    error messages

--- src/middlewares/authenticateProtection.js
+++ src/middlewares/authenticateProtection.js
@@ -5,13 +5,13 @@ const authenticateProtection = (req, res, next) => {
   const token = req.header("token");
 
   if (!token) {
-    return res.status(403).send("Un token es requerido para la autenticacion");
+    return res.status(403).send("Se requiere ser un usuario registrado");
   }
   try {
     const decoded = jwt.verify(token, TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Token invalido");
+    return res.status(401).send("Usuario invalido");
   }
   return next();
 };
