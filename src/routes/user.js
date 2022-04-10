e3ef360fa19d9d2beeb8e4a2408c36d14fb0dbe8
Hernán David Figueroa <davshn@gmail.com>
2022-04-09 23:58:40 -0500

    verify version

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,23 +13,29 @@ const {
 } = require("../middlewares/validators/user");
 
 const router = Router();
-const { TOKEN_KEY } = process.env;
+const { TOKEN_KEY, VERSION } = process.env;
 
 router.post("/register", validateRegister, async (req, res) => {
   const salt = await bcrypt.genSalt(10);
 
   try {
-    const newUser = await User.create({
-      dateOfBirth: req.body.dateOfBirth,
-      name: req.body.name,
-      lastname: req.body.lastname,
-      email: req.body.email.toLowerCase(),
-      password: await bcrypt.hash(req.body.password, salt),
-    });
+    const version = req.body.version;
 
-    verification(newUser.email);
+    if (version !== VERSION) {
+      res.status(409).send("Actualiza tu aplicacion");
+    } else {
+      const newUser = await User.create({
+        dateOfBirth: req.body.dateOfBirth,
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email.toLowerCase(),
+        password: await bcrypt.hash(req.body.password, salt),
+      });
 
-    res.status(200).send("Usuario creado con exito");
+      verification(newUser.email);
+
+      res.status(200).send("Usuario creado con exito");
+    }
   } catch (error) {
     res.status(400).send("Error en la creacion de usuario " + error);
   }
@@ -39,23 +45,28 @@ router.post("/login", validateLogin, async (req, res) => {
   try {
     const email = req.body.email.toLowerCase();
     const password = req.body.password;
+    const version = req.body.version;
 
-    const user = await User.findOne({ where: { email } });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const token = jwt.sign({ id: user.id, email: user.email }, TOKEN_KEY, {
-        expiresIn: "2h",
-      });
-
-      user.token = token;
-      const loggedUser = {
-        token: user.token,
-        id: user.id,
-      };
-
-      res.status(200).json(loggedUser);
+    if (version !== VERSION) {
+      res.status(409).send("Actualiza tu aplicacion");
     } else {
-      res.status(402).send("Usuario o contraseña incorrectos");
+      const user = await User.findOne({ where: { email } });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ id: user.id, email: user.email }, TOKEN_KEY, {
+          expiresIn: "2h",
+        });
+
+        user.token = token;
+        const loggedUser = {
+          token: user.token,
+          id: user.id,
+        };
+
+        res.status(200).json(loggedUser);
+      } else {
+        res.status(402).send("Usuario o contraseña incorrectos");
+      }
     }
   } catch (error) {
     res.status(405).send("Error en el login");
