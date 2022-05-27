51d1def741d7d17427666f526706328b09ec3591
Hernán David Figueroa <davshn@gmail.com>
2022-05-26 22:34:30 -0500

    Logs to refresh token

--- src/routes/user.js
+++ src/routes/user.js
@@ -140,10 +140,12 @@ router.get("/refresh", versionProtection, refreshTokenProtection, async (req, re
     const refreshToken = req.header("RefreshToken");
     const userId = req.user.id;
     const deviceInfo = req.user.deviceInfo;
-
+    console.log(refreshToken)
+    console.log(await bcrypt.compare(refreshToken, deviceInfo))
+    console.log(user.deviceInfo)
     const user = await User.findOne({ where: { id: userId } });
     if (
-      (await bcrypt.compare(refreshToken, deviceInfo)) &&
+      await bcrypt.compare(refreshToken, deviceInfo) &&
       refreshToken === user.deviceInfo
     ) {
       const salt = await bcrypt.genSalt(10);
